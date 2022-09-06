c792bcf9e50a4ac7b5c01cbe8ad2169f83b35f94
gerogarzon <gerogarzon@gmail.com>
2022-09-06 12:06:18 -0300

    proteccting routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Main from "./components/main/Main";
 import Admin from "./components/admin/Admin";
 import Users from "./components/admin/Users/Users";
@@ -12,23 +12,31 @@ import SobreNosotros from "./pages/SobreNosotros/SobreNosotros";
 import Orders from './components/admin/Orders/Orders'
 import Error404 from './pages/404/Error404'
 import {CartProvider} from './context/CartContext'
+
 function App() {
+
+  const isAdmin = JSON.parse(localStorage.getItem("userToken"))
+  const isAdminRole = isAdmin.role;
+  console.log("ggg",isAdminRole)
+
   return (
     <>
     <CartProvider>
+      
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Main />} />
-          <Route path="/admin" element={<Admin />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/adminUsers" element={<Users/>} />
-          <Route path="/adminProducts" element={<Products />} />
-          <Route path="/adminOrders" element={<Orders/>} />
+          <Route path="/admin" element={(isAdminRole === "ADMIN_ROLE") ? (<Admin/>):(<Navigate to="/" />)} />
+          <Route path="/adminUsers" element={(isAdminRole === "ADMIN_ROLE") ? (<Users/>):(<Navigate to="/" />)}  />
+          <Route path="/adminProducts" element={(isAdminRole === "ADMIN_ROLE") ? (<Products />):(<Navigate to="/" />)} />
+          <Route path="/adminOrders" element={(isAdminRole === "ADMIN_ROLE") ? (<Orders/>):(<Navigate to="/" />)} />
           <Route path="/sobreNosotros" element={<SobreNosotros />} />    
           <Route path="/Error404" element={<Error404 />} />         
         </Routes>
       </BrowserRouter>
+      
       </CartProvider>
       
     </>
