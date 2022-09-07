56b847ad2934ddd89e6eb15c0aa752a2cd5798a1
gerogarzon <gerogarzon@gmail.com>
2022-09-07 14:44:44 -0300

    add ivan information

--- src/pages/SobreNosotros/SobreNosotros.jsx
+++ src/pages/SobreNosotros/SobreNosotros.jsx
@@ -4,6 +4,7 @@ import Header from "../../components/layout/header";
 import Footer from "../../components/layout/Footer";
 import "./sobreNosotros.css";
 import gero from "../../resources/gero.jpg";
+import ivan from "../../resources/ivan.jpeg";
 
 const SobreNosotros = () => {
   return (
@@ -24,20 +25,19 @@ const SobreNosotros = () => {
           </Card.Body>
         </Card>
 
-        {/* <Card className="sobreNosotros-items" style={{ width: "18rem" }}>
-          <Card.Img className="card-imagen" variant="top" src={gero} alt="creator image"/>
+         <Card className="sobreNosotros-items" style={{ width: "18rem" }}>
+          <Card.Img className="card-imagen" variant="top" src={ivan} alt="creator image"/>
           <Card.Body>
             <Card.Title>Ivan Roldan</Card.Title>
             <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+            Tengo más de 10 años de experiencia profesional. Tengo experiencia en finanzas y gestión de programas. En los últimos años me he especializado en generación de leads y manejo masivo de datos para generar ventas y nuevas oportunidades de negocio.Tengo pasión por la tecnología y esto me llevó a hacer un Bootcamp para convertirme en desarrollador Full Stack. Estoy buscando mi próxima oportunidad de trabajo.
             </Card.Text>
           </Card.Body>         
           <Card.Body>
-            <Card.Link className="cardLink" href="#" target="_blank">Card Link</Card.Link>
-            <Card.Link className="cardLink" href="#" target="_blank">Another Link</Card.Link>
+            <Card.Link className="cardLink" href="https://www.linkedin.com/in/iv%C3%A1n-el%C3%ADas-rold%C3%A1n-130b72174/" target="_blank">Linkedin</Card.Link>
+            <Card.Link className="cardLink" href="https://github.com/IERoldan" target="_blank">Github</Card.Link>
           </Card.Body>
-        </Card> */}
+        </Card> 
       </div>
      
 
